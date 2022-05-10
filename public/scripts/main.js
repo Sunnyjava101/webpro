7fd967835dd59313fcd5f17e0543ff5e36f30f8d
sunnysingh <sunsinghmultani1@gmail.com>
2022-05-10 15:49:01 -0400

    some updates

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,42 +1,61 @@
-class User {
-  constructor(email, userName, pswd) {
-	//changed attribute i previously had
-    this.userEmail = email;
-    this.userName = userName;
-    this.setUserPassword(pswd);
-  }
-  getUserEmail() {
-    return this.userEmail;
-  }
-  getUserName() {
-    return this.userName;
-  }
-  getUserPassword() {
-    return this.userPassword;
-  }
-  //set methods
-  setUserEmail(email) {
-    this.userEmail = email;
-  }
-  setUserName(userName) {
-    this.userName = userName;
-  }
-  setUserPassword(pswd) {
-	  this.userPassword = pswd;
+const nav = document.querySelector('nav');
+if(getCurrentUser()) {
+  nav.innerHTML = `
+    <ul>
+      <li><a href="bmi.html">Calculate</a></li>
+      <li><a href="profile.html">Profile</a></li>
+      <li><a id="logout">Logout</a></li>
+    </ul>
+  `;
+} else {
+  nav.innerHTML = `
+    <ul>
+      <li><a href="bmi.html">Calculate</a></li>
+      <li><a href="login.html">Login</a></li>
+      <li><a href="register.html">Sign Up</a></li>
+    </ul>
+  `
+}
+
+// http://localhost:3000
+// Fetch method implementation:
+export async function fetchData(route = '', data = {}, methodType) {
+  const response = await fetch(`${route}`, {
+    method: methodType, // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  if(response.ok) {
+    return await response.json(); // parses JSON response into native JavaScript objects
+  } else {
+    throw await response.json();
   }
 }
 
-const form = document.getElementById("register");
-form.addEventListener('submit', createUser);
-
-function createUser(e) {
-  e.preventDefault();
-
-	let e = document.getElementById("email");
-	let p = document.getElementById("password");
-	let u = document.getElementById("username");
-	
-	let user1 = new User(e, p, u);
-	console.log(user1);
+export function setCurrentUser(user) {
+  localStorage.setItem('user', JSON.stringify(user));
 }
 
+export function removeCurrentUser() {
+  localStorage.removeItem('user')
+}
+
+export function getCurrentUser() {
+  return JSON.parse(localStorage.getItem('user'));
+}
+
+export const logoutBtn = document.getElementById("logout");
+if(logoutBtn) logoutBtn.addEventListener('click', logout)
+
+export function logout() {
+  removeCurrentUser();
+  window.location.href = "login.html";
+}
